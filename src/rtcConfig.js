1838011b17c522633d3a6bbb944f0a56ac3fb8d0
Garox <gabriel@garox.org>
2018-02-07 15:32:02 -0300

    ice servers

--- src/rtcConfig.js
+++ src/rtcConfig.js
@@ -1,11 +1,13 @@
 export default {
     "iceServers": [
       {
+        "urls": "stun:stun.l.google.com:19302"
+      }, {
+        "urls": "stun:stun.stunprotocol.org:3478"
+      }, {
         "urls": ["stun:stun.l.google.com:19302", "stun:stun2.l.google.com:19302", "stun:stun1.l.google.com:19302", "stun:stun3.l.google.com:19302", "stun:stun4.l.google.com:19302"]
       },{
         "urls": "stun:stun.services.mozilla.com:3478"
-      }, {
-        "urls": "stun:stun.stunprotocol.org:3478"
       }
     ]
   }
